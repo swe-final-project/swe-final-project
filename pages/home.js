feda4450f181e1b1e203558ecf2bb299949b01b4
vjordan-cs <vjordan.cs@gmail.com>
2023-04-17 18:26:57 -0500

    working api calls, output is formatted

--- pages/home.js
+++ pages/home.js
@@ -1,14 +1,101 @@
-export default function Home() {
-  return (
-    <div className="h-screen">
-      <Head>
-        <title>Home</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+import Header from "@/components/header";
+import Textbox from "@/components/textbox";
+import Head from "next/head";
+import { useState } from "react";
 
-      <main className="h-screen">
-        <h1 className="text-6xl font-bold text-center mt-20">Home</h1>
-      </main>
+export default function Home() {
+  const [assignmentInput, setAssignmentInput] = useState("");
+  const [result, setResult] = useState();
+  const [loading, setLoading] = useState(false);
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    setLoading(true);
+
+    try {
+      const response = await fetch("/api/hello", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ assignment: assignmentInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
+      }
+
+      setResult(data.result);
+      setAssignmentInput("");
+    } catch (error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  function renderBulletPoints(output) {
+    const lines = output.split("\n").filter((line) => line.trim() !== "");
+
+    function boldPhrases(line) {
+      const phrasesToBold = [
+        "technical requirements:",
+        "formatting and submission requirements:",
+      ];
+
+      let modifiedLine = line;
+
+      phrasesToBold.forEach((phrase) => {
+        const regex = new RegExp(phrase, "gi");
+        modifiedLine = modifiedLine.replace(
+          regex,
+          (match) => `<strong>${match}</strong>`
+        );
+      });
+
+      return modifiedLine;
+    }
+
+    return (
+      <ul>
+        {lines.map((line, index) => (
+          <li
+            key={index}
+            dangerouslySetInnerHTML={{ __html: boldPhrases(line) }}
+          ></li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <div className="h-screen bg-pool bg-cover bg-center p-6">
+      <Header />
+      <div className="bg-white  m-auto backdrop-blur-lg bg-opacity-20 rounded-3xl p-8">
+        <h1 className="text-xl">Welcome!</h1>
+        <p>Paste your assignment instructions below.</p>
+        <h3>Name my pet</h3>
+        <form onSubmit={onSubmit}>
+          <Textbox
+            value={assignmentInput}
+            onChange={(e) => setAssignmentInput(e.target.value)}
+          />
+          <input type="submit" value="Generate names" />
+        </form>
+
+        <div>{result && renderBulletPoints(result)}</div>
+        {loading && (
+          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 bg-black">
+            <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-red-400"></div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
