2f2674d3f8177a0bb3ce818f856f5152fb699da2
vjordan-cs <vjordan.cs@gmail.com>
2023-04-16 20:29:34 -0500

    remove head from home.js

--- pages/home.js
+++ pages/home.js
@@ -1,11 +1,6 @@
 export default function Home() {
   return (
     <div className="h-screen">
-      <Head>
-        <title>Home</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className="h-screen">
         <h1 className="text-6xl font-bold text-center mt-20">Home</h1>
       </main>
