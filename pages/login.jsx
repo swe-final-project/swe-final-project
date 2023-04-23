65e226637b56d3469a9b18b8fca94fb3ebf4a163
Jonathan <jonathanu064@yahoo.com>
2023-04-23 17:42:37 -0500

    removed elements besides google and other login

--- pages/login.jsx
+++ pages/login.jsx
@@ -26,68 +26,12 @@ export default function login (){
                   <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
                     Sign in to your account
                   </h2>
-                  <p className="mt-2 text-sm leading-6 text-gray-500">
-                    Not a member?{' '}
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                      Start a 14 day free trial
-                    </a>
-                  </p>
+                  
                 </div>
     
                 <div className="mt-10">
                   <div>
-                    <form action="#" method="POST" className="space-y-6">
-                      <div>
-                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                          Email address
-                        </label>
-                        <div className="mt-2">
-                          <input
-                            id="email"
-                            name="email"
-                            type="email"
-                            autoComplete="email"
-                            required
-                            className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                          />
-                        </div>
-                      </div>
-    
-                      <div>
-                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                          Password
-                        </label>
-                        <div className="mt-2">
-                          <input
-                            id="password"
-                            name="password"
-                            type="password"
-                            autoComplete="current-password"
-                            required
-                            className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                          />
-                        </div>
-                      </div>
-    
-                      <div className="flex items-center justify-between">
-                        <div className="flex items-center">
-                          <input
-                            id="remember-me"
-                            name="remember-me"
-                            type="checkbox"
-                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                          />
-                          <label htmlFor="remember-me" className="ml-3 block text-sm leading-6 text-gray-700">
-                            Remember me
-                          </label>
-                        </div>
-    
-                        <div className="text-sm leading-6">
-                          <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                            Forgot password?
-                          </a>
-                        </div>
-                      </div>
+                    
     
                       <div>
                         <button
@@ -99,7 +43,7 @@ export default function login (){
         <Image src={"/assets/google.svg"} width="20" height={20}></Image>
         </button>
                       </div>
-                    </form>
+                    
                   </div>
     
                   <div className="mt-10">
