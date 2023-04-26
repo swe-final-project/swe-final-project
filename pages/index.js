993fa3455fb2eee8f90e41337d6e115d7e54bc4e
Jonathan <jonathanu064@yahoo.com>
2023-04-25 20:52:24 -0500

    changed image

--- pages/index.js
+++ pages/index.js
@@ -76,8 +76,8 @@ export default function login() {
         <div className="relative hidden w-0 flex-1 lg:block">
           <img
             className="absolute inset-0 h-full w-full object-cover"
-            src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
-            alt=""
+            src={"/assets/organize_desk.jpg"}
+            alt="person_working_happy"
           />
         </div>
       </div>
