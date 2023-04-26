1d866dadb0c164f329a78a338307aa93fc032392
Jonathan <jonathanu064@yahoo.com>
2023-04-26 00:07:54 -0500

    fixed tailwind logo

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function login() {
         <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
           <div className="mx-auto w-full max-w-sm lg:w-96">
             <div>
-              <Image
+              <img
                 className="h-10 w-auto"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt="Your Company"
